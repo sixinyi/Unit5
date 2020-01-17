abf02434098977c3e13b6742d849475dd59b460d
sixinyi <yangwenxuans@outlook.com>
2020-01-17 10:16:21 -0500

    Initial commit

---
+++ src/com/company/SquareDriver.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+public class SquareDriver {
+    public static void main(String[] args){
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter side length please: ");
+        double num = input.nextDouble();
+
+        Square n = new Square(num);
+        System.out.println(n);
+        System.out.println(n.calculateArea());
+    }
+}
