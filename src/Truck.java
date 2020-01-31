61661005da1af8483a7005d4e21a2432a8585626
sixinyi <yangwenxuans@outlook.com>
2020-01-31 11:21:09 -0500

    1/31

---
+++ src/Truck.java
@@ -0,0 +1,19 @@
+public class Truck {
+    private String truckID;
+    private double odometer;
+    private double mpg;
+    private double fuel;
+
+    private static final double CAPACITY = 20;
+    private static double totalFuel;
+
+    public Truck(String name){
+        truckID = name;
+    }
+
+    public Truck(double odo, double m, double fue){
+        odometer = odo
+    }
+
+
+}
