7ff206e4afb99558bb173d47cd544a8ab0e47964
sixinyi <yangwenxuans@outlook.com>
2020-02-05 20:40:40 -0500

    1/31

--- src/Truck.java
+++ src/Truck.java
@@ -1,3 +1,5 @@
+// 2020.2.3 Bruce
+
 public class Truck {
     private String truckID;
     private double odometer;
@@ -11,9 +13,78 @@ public class Truck {
         truckID = name;
     }
 
-    public Truck(double odo, double m, double fue){
-        odometer = odo
+    public Truck(String name, double odo, double m, double fue){
+        truckID = name;
+        odometer = odo;
+        mpg = m;
+        fuel = fue;
     }
 
+    public String getTruckID() {return truckID;}
+    public double getOdometer() {return odometer;}
+    public double getMpg() {return mpg;}
+    public double getFuel() {return fuel;}
 
+    //////下面的要改
+
+    public void setMpg(double mpg) {
+        this.mpg = mpg;
+    }
+
+    private boolean enoughFuel(double mile){
+        double need = mile/mpg;
+        if(fuel < need){
+            return false;
+        }
+        else{
+            return true;
+        }
+
+    }
+
+    public String drive(double mi){
+        if(enoughFuel(mi)){
+            odometer += mi;
+            fuel -= mi/mpg;
+            return "success";
+        }
+        else{
+            return "Truck " + truckID + " does not have enough fuel to drive 500.0 miles.";
+        }
+    }
+        /*
+        odometer += runMile;
+        fuel -= runMile / mpg;
+        if(fuel + runMile / mpg < runMile / mpg){
+            return "Truck " + truckID + " does not have enough fuel to drive 500.0 miles.";
+        }
+        else{
+            return "success";
+        }
+
+         */
+
+    public void fill(){
+        totalFuel += (CAPACITY-fuel);
+        fuel = 20;
+    }
+
+    public String fill(double f){
+        if(totalFuel <= 20){
+            fuel += f;
+            totalFuel += f;
+            return "success";
+        }else{
+            return "Truck " + truckID + ": Gallons exceeds tank capacity";
+        }
+    }
+
+    public static double getTotalFuel() {return totalFuel;}
+
+    public String toString() {
+        return "Truck: " + truckID + "\n" + "Odometer: " + odometer + "\n" + "Miles Per Gallon: " + mpg + "\n" + "Fuel: " + fuel;
+    }
 }
+
+
+
