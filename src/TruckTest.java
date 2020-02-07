00f8d62fa135fffb9a6e314c74faa44081f7ca24
sixinyi <yangwenxuans@outlook.com>
2020-02-07 08:56:13 -0500

    2/7 Bruce

--- src/TruckTest.java
+++ src/TruckTest.java
@@ -2,17 +2,21 @@ public class TruckTest {
     public static void main (String[] args){
         Truck t1 = new Truck("123ABC");
         Truck t2 = new Truck("456DEF",45500,18,10);
+
         t1.setMpg(15);
         t1.fill();
         t1.drive(150);
+
         t1.toString();
         t1.drive(60);
+
         t1.toString();
         t2.drive(500);
         t2.fill(30);
         t2.fill();
         t2.drive(300);
         t2.toString();
+
         System.out.println(t1.getTotalFuel()+t2.getTotalFuel());
     }
 }
