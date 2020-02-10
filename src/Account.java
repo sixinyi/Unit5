19c0cdef671f683587dc026ca02897d80bd91433
sixinyi <yangwenxuans@outlook.com>
2020-02-10 10:06:14 -0500

    2/10 Bruce

---
+++ src/Account.java
@@ -0,0 +1,60 @@
+// 2020/2/10 Bruce  This program will describe a checking account for a customer of a bank.
+
+public class Account {
+    private String first;
+    private String last;
+    private int acctNum;
+    private double balance;
+
+    public Account (String first,String last,double balance) {
+        acctNum = (int) (Math.random() * 999 + 1);
+        this.first = first;
+        this.last = last;
+        this.balance = balance;
+    }
+
+    public Account(Account recall){
+        this(recall.first,recall.last,recall.balance);
+    }
+    public Account(){
+        acctNum = (int)(Math.random()*999+1);
+        this.first = "";
+        this.last = "";
+        this.balance = 0;
+    }
+
+    public void setFirst(String first){
+        this.first = first;
+    }
+    public void setLast(String last){
+        this.last = last;
+    }
+    public String getFirst(){
+        return first;
+    }
+    public String getLast(){
+        return last;
+    }
+    public int getAcctNum(){
+        return acctNum;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public void deposit(double num){
+        balance += num;
+    }
+
+    public boolean withdrawal(double num){
+        if(balance - num >= 0) {
+            balance -= num;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public String toString(){
+        return "Customer Name: " + first + " " + last + "\n" + "Account #: " + acctNum + "\n" + "Current Balance: $" + balance;
+    }
+}
